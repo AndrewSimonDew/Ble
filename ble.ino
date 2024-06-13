2f8c81b490b80dfbf3593373c337bdbe6d0b6335
Andrex <simonandris2010@gmail.com>
2024-06-13 20:06:51 +0200

    fixed ble.ino

--- ble.ino
+++ ble.ino
@@ -1,10 +1,10 @@
 #include <SoftwareSerial.h>
 SoftwareSerial HM10(2, 3);
 int data;
-#define red 13
-#define yellow 12
-#define green 11
-#define blue 10
+int red = 13;
+int yellow = 12;
+int green = 11;
+int blue = 10;
 void setup() {
   Serial.begin(9600);
   HM10.begin(9600);
@@ -17,39 +17,43 @@ void setup() {
 void loop() {
   HM10.listen();
   while (HM10.available() > 0) {
-    Serial.println(HM10.read());
     data = HM10.read();
     if (data == 1) {
-      Serial.println("1!!!!");
+      Serial.println("red");
       digitalWrite(red, HIGH);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(green, LOW);
+      digitalWrite(blue, LOW);
     } else if (data == 2) {
+      Serial.println("yellow");
       digitalWrite(red, LOW);
-      digitalWrite(red, HIGH);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
+      digitalWrite(yellow, HIGH);
+      digitalWrite(green, LOW);
+      digitalWrite(blue, LOW);
     } else if (data == 3) {
+      Serial.println("greem");
       digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, HIGH);
-      digitalWrite(red, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(green, HIGH);
+      digitalWrite(blue, LOW);
     } else if (data == 4) {
+      Serial.println("blue");
       digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, HIGH);
+      digitalWrite(yellow, LOW);
+      digitalWrite(green, LOW);
+      digitalWrite(blue, HIGH);
     } else if (data == 5) {
+      Serial.println("none");
       digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(green, LOW);
+      digitalWrite(blue, LOW);
     } else if (data == 6) {
+      Serial.println("all");
       digitalWrite(red, HIGH);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
-      digitalWrite(red, LOW);
+      digitalWrite(yellow, HIGH);
+      digitalWrite(green, HIGH);
+      digitalWrite(blue, HIGH);
     }
   }
 }
